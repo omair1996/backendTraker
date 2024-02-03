40d3249ad4ed00d797eb55634caf95309fee9045
omair1996 <umaibribrahim07@gmail.com>
2024-02-03 16:16:11 +0100

    initial commit

--- index.js
+++ index.js
@@ -13,30 +13,24 @@ mongoose
     console.error(`error connecting to the db.${error}`);
   });
 
-const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      unique: true,
-    },
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
   },
-  { versionKey: false }
-);
+});
 
 const User = mongoose.model("User", userSchema);
 
-const exercisesSchema = new mongoose.Schema(
-  {
-    username: String,
-    description: String,
-    duration: Number,
-    date: Date,
-    userId: String,
-  },
-  { versionKey: false }
-);
+const exercisesSchema = new mongoose.Schema({
+  username: String,
+  description: { type: String, require: true },
+  duration: Number,
+  date: Date,
+  userId: { type: String, require: true },
+});
 
-const Exercises = mongoose.model("Exercises", exercisesSchema);
+const Exercise = mongoose.model("Exercise", exercisesSchema);
 
 app.use(cors());
 app.use(express.static("public"));
@@ -46,8 +40,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/users", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+  const foundUser = await User.find();
+  res.send(foundUser);
 });
 
 app.post("/api/users", async (req, res) => {
@@ -63,74 +57,76 @@ app.post("/api/users", async (req, res) => {
   res.json(user);
 });
 
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.json({ message: "no user found " });
+    } else {
+      const exercisesObj = new Exercise({
+        userId: user._id,
+        description,
+        duration,
+        date: date ? new Date(date) : new Date(),
+      });
+
+      const exercises = await exercisesObj.save();
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercises.description,
+        duration: exercises.duration,
+        date: new Date(exercises.date).toDateString(),
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.send("Sorry an error occured");
+  }
+});
+
 app.get("/api/users/:_id/logs", async (req, res) => {
-  let { from, to, limit } = req.query;
-  const userId = req.params._id;
+  const { from, to, limit } = req.query;
+  const id = req.params._id;
 
-  const founduser = await User.findById(userId);
-  if (!founduser) res.json({ message: "no user found " });
-
-  let filter = { userId };
-  let dateFilter = {};
+  const user = await User.findById(id);
+  if (!user) {
+    res.send("no user found ");
+    return;
+  }
+  let dateObj = {};
 
   if (from) {
-    dateFilter["$gte"] = new Date(from);
+    dateObj["$gte"] = new Date(from);
   }
 
   if (to) {
-    dateFilter["$lte"] = new Date(to);
+    dateObj["$lte"] = new Date(to);
   }
+
+  let filter = {
+    userId: id,
+  };
 
   if (from || to) {
-    filter.date = dateFilter;
+    filter.date = dateObj;
   }
 
-  if (!limit) {
-    limit = 100;
-  }
-  let exercises = await Exercises.find(filter).limit(limit);
-  exercises = exercises.map((exercise) => {
-    return {
-      description: exercise.description,
-      duration: exercise.duration,
-      date: exercise.date.toDateString(),
-    };
-  });
+  const exercises = await Exercise.find(filter).limit(limit ?? 500);
+
+  const logs = exercises.map((exercises) => ({
+    description: exercises.description,
+    duration: exercises.duration,
+    date: exercises.date.toDateString(),
+  }));
+
   res.json({
-    username: founduser.username,
+    username: user.username,
     count: exercises.length,
-    _id: userId,
-    log: exercises,
-  });
-});
-
-app.post("/api/users/:_id/exercises", async (req, res) => {
-  let { description, duration, date } = req.body;
-  const userid = req.body[":_id"];
-  const founduser = await User.findById(userid);
-
-  if (!founduser) res.json({ message: "no user found " });
-
-  if (!date) {
-    date = new Date();
-  } else {
-    date = new Date(date);
-  }
-
-  await Exercises.create({
-    username: founduser.username,
-    description,
-    duration,
-    date,
-    userid,
-  });
-
-  res.send({
-    username: founduser.username,
-    description,
-    duration,
-    date: date.toDateString(),
-    _id: userid,
+    _id: user._id,
+    log: logs,
   });
 });
 
